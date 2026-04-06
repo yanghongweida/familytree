5c3316792b302300314c2cb761655a39ed455643
yanghongweida <124655173+yanghongweida@users.noreply.github.com>
2026-04-06 17:33:00 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   webpack: (config) => {
     // 关键配置：客户端打包时忽略 Node.js 原生模块
+    config.resolve = config.resolve || {};
     config.resolve.fallback = {
       ...config.resolve.fallback,
       fs: false,
